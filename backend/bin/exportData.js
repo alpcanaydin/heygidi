34f85d276b591d903bd324dbc45c2f27679b3972
alpcanaydin <alpcan@alpcanaydin.com>
2017-01-28 00:38:48 +0300

    React added.

--- backend/bin/exportData.js
+++ backend/bin/exportData.js
@@ -12,7 +12,7 @@ const client = new elasticsearch.Client({
   host: esParams.host
 });
 
-const DATA_FOLDER = `${__dirname}/../app/data`;
+const DATA_FOLDER = `${__dirname}/../../app/src/data`;
 const YEARS = [
   2017, 2016, 2015, 2014, 2013, 2012, 2011,
   2010, 2009, 2008, 2007, 2006, 2005, 2004,
